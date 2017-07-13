a6a49065c104fe4f6b0ca647d3f6b73214c6af0b
tlandegger <landegger.t@husky.neu.edu>
2017-07-12 22:18:24 -0400

    update connection bug

--- test/app.js
+++ test/app.js
@@ -7,7 +7,7 @@ app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
     
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+if(process.env.MLAB_USER) { // check if running remotely
 	var username = process.env.MLAB_USER; // get from environment
 	var password = process.env.MLAB_PASS;
 	connectionString = 'mongodb://' + username + ':' + password;
